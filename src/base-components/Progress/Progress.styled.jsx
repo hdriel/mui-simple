bd67964ca18d124f9e926993dedf93894a05d6d3
hdriel <hdriel@gmail.com>
2023-05-14 22:37:29 +0300

    add CircularProgress

---
+++ src/base-components/Progress/Progress.styled.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { styled } from "@mui/material/styles";
+import {
+  LinearProgress as MuiLinearProgress,
+  CircularProgress as MuiCircularProgress,
+  Box as MuiBox,
+  Typography as MuiTypography,
+} from "@mui/material";
+
+export const LinearProgress = styled(MuiLinearProgress, {
+  shouldForwardProp: (propName) => !["customColor"].includes(propName),
+})``;
+
+const BoxWrapper = styled(MuiBox)`
+  position: relative;
+  display: inline-flex;
+`;
+
+const CenterBoxContent = styled(MuiBox)`
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const CircularProgress = styled(
+  ({ showProgress, variant, value, ...props }) => (
+    <BoxWrapper>
+      <MuiCircularProgress
+        value={value}
+        variant={value && variant === undefined ? "determinate" : variant}
+        {...props}
+      />
+      <CenterBoxContent>
+        <MuiTypography variant="caption" component="div" color="text.secondary">
+          {showProgress && value !== undefined ? `${Math.round(value)}%` : ""}
+        </MuiTypography>
+      </CenterBoxContent>
+    </BoxWrapper>
+  ),
+  {
+    shouldForwardProp: (propName) => !["customColor"].includes(propName),
+  }
+)`
+  &.MuiCircularProgress-root {
+    color: ${(props) => props.customColor};
+  }
+`;
