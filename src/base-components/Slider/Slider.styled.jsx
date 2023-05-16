a383de76f25474839079d0e4dea7a6d6d27b4645
Hadriel Benjo <hdriel@gmail.com>
2023-05-16 18:56:09 +0300

    update Slider

--- src/base-components/Slider/Slider.styled.jsx
+++ src/base-components/Slider/Slider.styled.jsx
@@ -1,6 +1,7 @@
 import {
   Slider as MuiSlider,
   Typography as MuiTypography,
+  alpha,
 } from "@mui/material";
 import { styled } from "@mui/material/styles";
 export { Grid, Box } from "@mui/material";
@@ -8,7 +9,25 @@ export { Grid, Box } from "@mui/material";
 export const Slider = styled(MuiSlider, {
   shouldForwardProp: (propName) =>
     !["startIcon", "endIcon", "customColor"].includes(propName),
-})``;
+})`
+  & .MuiSlider-track,
+  & .MuiSlider-rail,
+  & .MuiSlider-thumb {
+    color: ${(props) => props.customColor};
+  }
+
+  & .MuiSlider-thumb:hover,
+  & .Mui-focusVisible {
+    box-shadow: ${(props) =>
+      props.customColor && `0px 0px 0px 8px ${alpha(props.customColor, 0.16)}`};
+  }
+
+  & .MuiSlider-thumb.Mui-active {
+    box-shadow: ${(props) =>
+      props.customColor &&
+      `0px 0px 0px 14px ${alpha(props.customColor, 0.16)}`};
+  }
+`;
 
 export const SliderLabel = styled(({ ...props }) => (
   <MuiTypography gutterBottom {...props} />
