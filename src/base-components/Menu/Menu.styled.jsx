de196abead375fbe3d17b3383e025d13cfa17da8
hdriel <hdriel@gmail.com>
2023-05-19 17:55:01 +0300

    add menu component

--- src/base-components/Menu/Menu.styled.jsx
+++ src/base-components/Menu/Menu.styled.jsx
@@ -1,7 +1,7 @@
 import {
   MenuList as MuiMenuList,
   MenuItem as MuiMenuItem,
-  ListItemIcon as MuiListItemIcon,,
+  ListItemIcon as MuiListItemIcon,
   ListItemText as MuiListItemText,
 } from "@mui/material";
 import { styled } from "@mui/material/styles";
