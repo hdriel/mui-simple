d1ec50dc136db2d3442510e992c4bc9e139e7f7b
hdriel <hdriel@gmail.com>
2023-05-22 00:43:00 +0300

    update SpeedDial component stories

--- src/base-components/SpeedDial/SpeedDial.styled.jsx
+++ src/base-components/SpeedDial/SpeedDial.styled.jsx
@@ -6,6 +6,7 @@ import {
 } from "@mui/material";
 import { styled, css } from "@mui/material/styles";
 
+export const SpeedDialIcon = MuiSpeedDialIcon; // note: styled on MuiSpeedDialIcon will break his flip icon animation!!
 export const SpeedDial = styled(
   ({ top, bottom, right, left, ...props }) => (
     <MuiSpeedDial sx={{ top, bottom, right, left }} {...props} />
@@ -41,4 +42,3 @@ export const SpeedDial = styled(
     }}
 `;
 export const SpeedDialAction = styled(MuiSpeedDialAction)``;
-export const SpeedDialIcon = styled(MuiSpeedDialIcon)``;
