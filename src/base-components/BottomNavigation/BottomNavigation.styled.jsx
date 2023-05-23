9f7a78061fdf686dcea1c68d04f19e4983c92e10
Hadriel Benjo <hdriel@gmail.com>
2023-05-23 19:13:42 +0300

    add BottomNavigation component

---
+++ src/base-components/BottomNavigation/BottomNavigation.styled.jsx
@@ -0,0 +1,40 @@
+import {
+  BottomNavigation as MuiBottomNavigation,
+  BottomNavigationAction as MuiBottomNavigationAction,
+} from "@mui/material";
+import { styled } from "@mui/material/styles";
+import { numberToPx } from "../../utils/helpers";
+import Paper from "../Paper/Paper";
+import _ from "lodash";
+
+export const BottomNavigation = styled(
+  ({ width, elevation, fixedToTop, fixedToBottom, position, ...props }) => (
+    <Paper
+      sx={{
+        width,
+        ...((fixedToTop || fixedToBottom) && {
+          position: position ?? "fixed",
+          left: 0,
+          right: 0,
+          top: fixedToTop ? 0 : undefined,
+          bottom: fixedToBottom ? 0 : undefined,
+        }),
+      }}
+      elevation={elevation}
+    >
+      <MuiBottomNavigation {...props} />
+    </Paper>
+  ),
+  {
+    shouldForwardProp: (propName) =>
+      !["muiColor", "customColor"].includes(propName),
+  }
+)`
+  & .MuiBottomNavigationAction-root.Mui-selected {
+    color: ${(props) =>
+      _.get(props, `theme.palette.${props.muiColor}.main`) ??
+      _.get(props, `theme.palette.${props.muiColor}`) ??
+      props.customColor};
+  }
+`;
+export const BottomNavigationAction = MuiBottomNavigationAction;
