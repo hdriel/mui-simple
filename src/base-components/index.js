198006bb922c20b94c8006d57f6a83f452dcb1f1
Hadriel Benjo <hdriel@gmail.com>
2023-05-24 19:56:14 +0300

    update index

--- src/base-components/index.js
+++ src/base-components/index.js
@@ -1,9 +1,12 @@
+export { default as Accordion } from "./Accordion/Accordion";
+export { default as Alert } from "./Alert/Alert";
 export { default as AppBar } from "./AppBar/AppBar";
 export { default as Avatar } from "./Avatar/Avatar";
-export { default as Alert } from "./Alert/Alert";
 
 export { default as Backdrop } from "./Backdrop/Backdrop";
 export { default as Badge } from "./Badge/Badge";
+export { default as BottomNavigation } from "./BottomNavigation/BottomNavigation";
+export { default as Breadcrumbs } from "./Breadcrumbs/Breadcrumbs";
 export { default as Button } from "./Button/Button";
 export { default as ButtonGroup } from "./Button/ButtonGroup";
 
@@ -16,6 +19,15 @@ export { default as Divider } from "./Divider/Divider";
 export { default as Fab } from "./FloatingActionButton/FloatingActionButton";
 
 export { default as Input } from "./Input/Input";
+
+export { default as Link } from "./Link/Link";
+export { default as List } from "./List/List";
+export { default as CheckList } from "./List/CheckList";
+
+export { default as Menu } from "./Menu/Menu";
+export { default as ContextMenu } from "./Menu/ContextMenu";
+
+export { default as Paper } from "./Paper/Paper";
 export { default as PasswordInput } from "./PasswordInput/PasswordInput";
 export { default as CircularProgress } from "./Progress/CircularProgress/CircularProgress";
 export { default as LinearProgress } from "./Progress/LinearProgress/LinearProgress";
@@ -23,10 +35,11 @@ export { default as LinearProgress } from "./Progress/LinearProgress/LinearProgr
 export { default as RadioButtonsGroup } from "./RadioButtonsGroup/RadioButtonsGroup";
 export { default as Rating } from "./Rating/Rating";
 
-export { default as Snackbar } from "./Snackbar/Snackbar";
-export { default as Switch } from "./Switch/Switch";
+export { default as Skeleton } from "./Skeleton/Skeleton";
 export { default as Slider } from "./Slider/Slider";
+export { default as Snackbar } from "./Snackbar/Snackbar";
 export { default as RangeSlider } from "./Slider/RangeSlider";
+export { default as Switch } from "./Switch/Switch";
 
 export { default as ToggleButtonGroup } from "./ToggleButtonGroup/ToggleButtonGroup";
 export { default as ToggleButtonGroups } from "./ToggleButtonGroup/ToggleButtonGroups";
