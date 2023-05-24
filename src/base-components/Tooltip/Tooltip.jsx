a28967c72aca27cdfb7296b866c5c9163843e826
hdriel <hdriel@gmail.com>
2023-05-25 00:34:03 +0300

    update tooltip styles

--- src/base-components/Tooltip/Tooltip.jsx
+++ src/base-components/Tooltip/Tooltip.jsx
@@ -1,23 +1,43 @@
 import React, { isValidElement } from "react";
 import PropTypes from "prop-types";
 import { isForwardRef } from "react-is";
-import { Tooltip as MuiTooltip, Zoom } from "@mui/material";
+import { Tooltip as MuiTooltip, Zoom } from "./Tooltip.styled";
 import { CustomChildTooltipWrapper } from "./Tooltip.helper";
 import { TOOLTIP_PLACEMENTS } from "./Tooltip.consts";
 
-export default function Tooltip({ title, placement, children, ...props }) {
+export default function Tooltip({
+  title,
+  placement,
+  followCursor,
+  bgcolor,
+  color,
+  fontSize,
+  lineHeight,
+  children,
+  ...props
+}) {
   const isValidTooltipProps = title && isValidElement(children);
-
-  // useEffect(() => {
-  //   if (!isValidTooltipProps) console.warn("Invalid Tooltip children or title");
-  // }, [isValidTooltipProps]);
 
   return isValidTooltipProps ? (
     <MuiTooltip
       TransitionComponent={Zoom}
       title={title}
       arrow
+      followCursor={followCursor}
       placement={placement}
+      componentsProps={{
+        tooltip: {
+          sx: {
+            bgcolor,
+            color,
+            fontSize,
+            lineHeight,
+            "& .MuiTooltip-arrow": {
+              color: bgcolor,
+            },
+          },
+        },
+      }}
       {...props}
     >
       {isForwardRef(children) ? (
@@ -34,9 +54,19 @@ export default function Tooltip({ title, placement, children, ...props }) {
 Tooltip.propTypes = {
   title: PropTypes.string,
   placement: PropTypes.oneOf(TOOLTIP_PLACEMENTS),
+  followCursor: PropTypes.bool,
+  bgcolor: PropTypes.string,
+  color: PropTypes.string,
+  fontSize: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  lineHeight: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
 };
 
 Tooltip.defaultProps = {
   title: undefined,
   placement: "bottom",
+  followCursor: undefined,
+  bgcolor: undefined,
+  color: undefined,
+  fontSize: 16,
+  lineHeight: 1.2,
 };
