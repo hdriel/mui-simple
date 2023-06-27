aa6e58fd0fae077b7a52d6228cdd41c4cec65890
hdriel <hdriel@gmail.com>
2023-06-28 01:30:14 +0300

    update config

--- .storybook/main.js
+++ .storybook/main.js
@@ -15,6 +15,7 @@ const config = {
         '@storybook/addon-interactions',
         '@storybook/blocks',
         '@storybook/addon-docs',
+        '@storybook/preset-create-react-app',
         {
             name: '@storybook/addon-storysource',
             options: { loaderOptions: { injectStoryParameters: false } },
@@ -27,21 +28,30 @@ const config = {
             })
         );
 
-        // todo : remove this, no need
-        config.module.rules?.push({
-            test: /\.[jt]sx?$/,
+        const typescriptRule = {
+            test: /\.(ts|tsx)$/,
             exclude: [path.resolve(__dirname, '..', 'node_modules')],
             use: [
                 { loader: 'babel-loader' },
                 {
-                    loader: 'ts-loader',
+                    loader: require.resolve('ts-loader'),
                     options: {
                         configFile: path.resolve(__dirname, '..', 'tsconfig.json'),
                         transpileOnly: true,
                     },
                 },
             ],
-        });
+        };
+        config.module.rules.push(typescriptRule);
+
+        // Add the peer dependencies to the rule's include array
+        typescriptRule.include ||= [];
+        typescriptRule.include.push(
+            path.resolve(__dirname, '../node_modules/react'),
+            path.resolve(__dirname, '../node_modules/react-dom'),
+            path.resolve(__dirname, '../node_modules/@mui'),
+            path.resolve(__dirname, '../node_modules/@emotion')
+        );
 
         return config;
     },
