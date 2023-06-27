f4e6070d82db71ac78b4ff33d9d6bf0ef4030375
hdriel <hdriel@gmail.com>
2023-06-27 22:58:20 +0300

    update config

--- rollup.config.js
+++ rollup.config.js
@@ -1,33 +1,24 @@
 // https://medium.com/self-learning/build-react-library-by-rollup-5680252e1aee
-// import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import json from '@rollup/plugin-json';
 import postcss from 'rollup-plugin-postcss';
 import dts from 'rollup-plugin-dts';
-import { terser } from 'rollup-plugin-terser';
 import del from 'rollup-plugin-delete';
-import { babel } from '@rollup/plugin-babel';
-import { fileURLToPath } from 'url';
-import path, { dirname } from 'path';
-import { uglify } from 'rollup-plugin-uglify';
 import generatePackageJson from 'rollup-plugin-generate-package-json';
+import { terser } from 'rollup-plugin-terser';
+import { babel } from '@rollup/plugin-babel';
+import { uglify } from 'rollup-plugin-uglify';
 
 import { createRequire } from 'node:module';
 const requireFile = createRequire(import.meta.url);
 const packageJson = requireFile('./package.json');
 const isProd = process.env.NODE_ENV === 'production';
 const sourcemap = isProd ? undefined : 'inline';
-console.log('process.env.NODE_ENV', process.env.NODE_ENV);
 
-function getDirname(importMetaUrl) {
-    const __filename = fileURLToPath(importMetaUrl);
-    return dirname(__filename);
-}
-const __dirname = getDirname(import.meta.url);
-const babelConfigFilePath = path.resolve(__dirname, '.babelrc');
-const buildESM = false;
+const ESM = true;
 
 export default [
     {
@@ -35,7 +26,7 @@ export default [
         output: [
             { sourcemap, format: 'cjs', file: packageJson.main },
             // ES2015 modules version so consumers can tree-shake
-            ...(buildESM ? { sourcemap, format: 'esm', file: packageJson.module } : []),
+            ...(ESM ? [{ sourcemap, format: 'esm', file: packageJson.module }] : []),
         ],
         external: [
             '@emotion/react',
@@ -48,7 +39,7 @@ export default [
         ],
         plugins: [
             del({ targets: 'dist/*' }),
-            // peerDepsExternal(),
+            peerDepsExternal(),
             resolve({ extensions: ['.js', '.jsx', '.ts', '.tsx'] }),
             commonjs(),
             json(),
@@ -56,25 +47,30 @@ export default [
             typescript({ tsconfig: './tsconfig.json' }),
             postcss({ minimize: true, extensions: ['.css', '.less', '.scss'] }),
             ...(isProd ? [terser(), uglify()] : []),
-            // generatePackageJson({
-            //     outputFolder: 'dist',
-            //     baseContents: (pkg) => ({
-            //         // ...pkg,
-            //         name: pkg.name,
-            //         ...(buildESM && { type: 'module', module: 'bundles/bundle.esm.js' }),
-            //         main: 'bundles/bundle.js',
-            //         types: 'bundles/bundle.d.ts',
-            //         files: ['bundles/*'],
-            //         dependencies: undefined,
-            //         devDependencies: undefined,
-            //         scripts: undefined,
-            //     }),
-            // }),
+            generatePackageJson({
+                outputFolder: 'dist',
+                baseContents: (pkg) => ({
+                    name: pkg.name,
+                    version: pkg.version,
+                    description: pkg.description,
+                    license: pkg.license,
+                    author: pkg.author,
+                    keywords: pkg.keywords,
+                    bugs: pkg.bugs,
+                    homepage: pkg.homepage,
+                    publishConfig: pkg.publishConfig,
+                    repository: pkg.repository,
+                    ...(ESM && { type: pkg.type, module: pkg.module?.replace('dist/') }),
+                    main: pkg.main.replace('dist/'),
+                    types: pkg.types.replace('dist/'),
+                    files: ['bundles/*'],
+                }),
+            }),
         ],
     },
     {
-        input: 'dist/bundles/bundle.d.ts',
-        output: [{ file: 'dist/bundles/bundle.d.ts', format: 'es' }],
+        input: 'dist/bundles/lib/index.d.ts',
+        output: [{ file: 'dist/bundles/lib/index.d.ts', format: 'es' }],
         plugins: [dts()],
         external: [/\.(css|less|scss)$/],
     },
--- src/index.ts
+++ src/index.ts
@@ -1,81 +1,81 @@
-export { default as Accordion } from './components/Accordion/Accordion';
-export { default as Alert } from './components/Alert/Alert';
-export { default as AppBar } from './components/AppBar/AppBar';
-export { default as InputAutocomplete } from './components/Autocomplete/InputAutocomplete';
-export { default as InputAutoCompleteAsync } from './components/Autocomplete/InputAutocompleteAsync';
-export { default as InputAutocompleteMultiple } from './components/Autocomplete/InputAutocompleteMultiple';
-export { default as InputGoogleAddress } from './components/Autocomplete/InputGoogleAddress';
-export { default as Avatar } from './components/Avatar/Avatar';
-
-export { default as Backdrop } from './components/Backdrop/Backdrop';
-export { default as Badge } from './components/Badge/Badge';
-export { default as BottomNavigation } from './components/BottomNavigation/BottomNavigation';
-export { default as Breadcrumbs } from './components/Breadcrumbs/Breadcrumbs';
-export { default as Button } from './components/Button/Button';
-export { default as ButtonGroup } from './components/Button/ButtonGroup';
-
-export { default as Checkbox } from './components/Checkbox/Checkbox';
-export { default as Chip } from './components/Chip/Chip';
-
-export { default as Dialog } from './components/Dialog/Dialog';
-export { default as Divider } from './components/Divider/Divider';
-export { default as Drawer } from './components/Drawer/Drawer';
-
-export { default as Fab } from './components/FloatingActionButton/FloatingActionButton';
-
-export { default as TextField } from './components/TextField/TextField';
-export { default as InputColor } from './components/TextField/InputColor';
-export { default as InputDate } from './components/TextField/InputDate';
-export { default as InputDateTime } from './components/TextField/InputDateTime';
-export { default as InputEmail } from './components/TextField/InputEmail';
-export { default as InputFile } from './components/TextField/InputFile';
-export { default as InputNumber } from './components/TextField/InputNumber';
-export { default as InputPassword } from './components/TextField/InputPassword';
-export { default as InputPattern } from './components/TextField/InputPattern';
-export { default as InputPhone } from './components/TextField/InputPhone';
-export { default as InputSearch } from './components/TextField/InputSearch';
-export { default as InputText } from './components/TextField/InputText';
-export { default as InputTime } from './components/TextField/InputTime';
-export { default as InputSelect } from './components/Select/InputSelect';
-export { default as InputMultipleSelect } from './components/Select/InputMultipleSelect';
-
-export { default as Link } from './components/Link/Link';
-export { default as List } from './components/List/List';
-export { default as CheckList } from './components/List/CheckList';
-
-export { default as Menu } from './components/Menu/Menu';
-export { default as ContextMenu } from './components/Menu/ContextMenu';
-
-export { default as Pagination } from './components/Pagination/Pagination';
-export { default as Paper } from './components/Paper/Paper';
-export { default as CircularProgress } from './components/Progress/CircularProgress/CircularProgress';
-export { default as LinearProgress } from './components/Progress/LinearProgress/LinearProgress';
-
-export { default as RadioButtonsGroup } from './components/RadioButtonsGroup/RadioButtonsGroup';
-export { default as Rating } from './components/Rating/Rating';
-export { RippleBox } from './components/RippleBox/RippleBox';
-
-export { default as Skeleton } from './components/Skeleton/Skeleton';
-export { default as Slider } from './components/Slider/Slider';
-export { default as RangeSlider } from './components/Slider/RangeSlider';
-export { default as Snackbar } from './components/Snackbar/Snackbar';
-export { default as SpeedDial } from './components/SpeedDial/SpeedDial';
-export { default as Stepper } from './components/Stepper/Stepper';
-export { default as MobileStepper } from './components/Stepper/MobileStepper';
-export { useSimpleStepper } from './components/Stepper/Stepper.utils';
-export { default as SVGIcon } from './components/SVGIcon/SVGIcon';
-export { default as Switch } from './components/Switch/Switch';
-
-export { Table } from './components/Table/Table';
-export { default as Tabs } from './components/Tabs/Tabs';
-export { default as Tab } from './components/Tabs/Tab';
-export { default as Timeline } from './components/Timeline/Timeline';
-export { default as ToggleButtonGroup } from './components/ToggleButtonGroup/ToggleButtonGroup';
-export { default as ToggleButtonGroups } from './components/ToggleButtonGroup/ToggleButtonGroups';
-export { default as Tooltip } from './components/Tooltip/Tooltip';
-export { default as TreeView } from './components/TreeView/TreeView';
-export * from './components/TreeView';
-export { default as Typography } from './components/Typography/Typography';
+// export { default as Accordion } from './components/Accordion/Accordion';
+// export { default as Alert } from './components/Alert/Alert';
+// export { default as AppBar } from './components/AppBar/AppBar';
+// export { default as InputAutocomplete } from './components/Autocomplete/InputAutocomplete';
+// export { default as InputAutoCompleteAsync } from './components/Autocomplete/InputAutocompleteAsync';
+// export { default as InputAutocompleteMultiple } from './components/Autocomplete/InputAutocompleteMultiple';
+// export { default as InputGoogleAddress } from './components/Autocomplete/InputGoogleAddress';
+// export { default as Avatar } from './components/Avatar/Avatar';
+//
+// export { default as Backdrop } from './components/Backdrop/Backdrop';
+// export { default as Badge } from './components/Badge/Badge';
+// export { default as BottomNavigation } from './components/BottomNavigation/BottomNavigation';
+// export { default as Breadcrumbs } from './components/Breadcrumbs/Breadcrumbs';
+// export { default as Button } from './components/Button/Button';
+// export { default as ButtonGroup } from './components/Button/ButtonGroup';
+//
+// export { default as Checkbox } from './components/Checkbox/Checkbox';
+// export { default as Chip } from './components/Chip/Chip';
+//
+// export { default as Dialog } from './components/Dialog/Dialog';
+// export { default as Divider } from './components/Divider/Divider';
+// export { default as Drawer } from './components/Drawer/Drawer';
+//
+// export { default as Fab } from './components/FloatingActionButton/FloatingActionButton';
+//
+// export { default as TextField } from './components/TextField/TextField';
+// export { default as InputColor } from './components/TextField/InputColor';
+// export { default as InputDate } from './components/TextField/InputDate';
+// export { default as InputDateTime } from './components/TextField/InputDateTime';
+// export { default as InputEmail } from './components/TextField/InputEmail';
+// export { default as InputFile } from './components/TextField/InputFile';
+// export { default as InputNumber } from './components/TextField/InputNumber';
+// export { default as InputPassword } from './components/TextField/InputPassword';
+// export { default as InputPattern } from './components/TextField/InputPattern';
+// export { default as InputPhone } from './components/TextField/InputPhone';
+// export { default as InputSearch } from './components/TextField/InputSearch';
+// export { default as InputText } from './components/TextField/InputText';
+// export { default as InputTime } from './components/TextField/InputTime';
+// export { default as InputSelect } from './components/Select/InputSelect';
+// export { default as InputMultipleSelect } from './components/Select/InputMultipleSelect';
+//
+// export { default as Link } from './components/Link/Link';
+// export { default as List } from './components/List/List';
+// export { default as CheckList } from './components/List/CheckList';
+//
+// export { default as Menu } from './components/Menu/Menu';
+// export { default as ContextMenu } from './components/Menu/ContextMenu';
+//
+// export { default as Pagination } from './components/Pagination/Pagination';
+// export { default as Paper } from './components/Paper/Paper';
+// export { default as CircularProgress } from './components/Progress/CircularProgress/CircularProgress';
+// export { default as LinearProgress } from './components/Progress/LinearProgress/LinearProgress';
+//
+// export { default as RadioButtonsGroup } from './components/RadioButtonsGroup/RadioButtonsGroup';
+// export { default as Rating } from './components/Rating/Rating';
+// export { RippleBox } from './components/RippleBox/RippleBox';
+//
+// export { default as Skeleton } from './components/Skeleton/Skeleton';
+// export { default as Slider } from './components/Slider/Slider';
+// export { default as RangeSlider } from './components/Slider/RangeSlider';
+// export { default as Snackbar } from './components/Snackbar/Snackbar';
+// export { default as SpeedDial } from './components/SpeedDial/SpeedDial';
+// export { default as Stepper } from './components/Stepper/Stepper';
+// export { default as MobileStepper } from './components/Stepper/MobileStepper';
+// export { useSimpleStepper } from './components/Stepper/Stepper.utils';
+// export { default as SVGIcon } from './components/SVGIcon/SVGIcon';
+// export { default as Switch } from './components/Switch/Switch';
+//
+// export { Table } from './components/Table/Table';
+// export { default as Tabs } from './components/Tabs/Tabs';
+// export { default as Tab } from './components/Tabs/Tab';
+// export { default as Timeline } from './components/Timeline/Timeline';
+// export { default as ToggleButtonGroup } from './components/ToggleButtonGroup/ToggleButtonGroup';
+// export { default as ToggleButtonGroups } from './components/ToggleButtonGroup/ToggleButtonGroups';
+// export { default as Tooltip } from './components/Tooltip/Tooltip';
+// export { default as TreeView } from './components/TreeView/TreeView';
+// export * from './components/TreeView';
+// export { default as Typography } from './components/Typography/Typography';
 
 export * from './utils';
 export { default as useElementSize } from './hooks/useElementSize';
