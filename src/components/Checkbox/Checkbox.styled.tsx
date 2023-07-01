5464d56e18a1458c133a1219f964a2fb9d6cdfb0
hdriel <hdriel@gmail.com>
2023-07-01 23:10:04 +0300

    update Checkbox.styled.tsx

--- src/components/Checkbox/Checkbox.styled.tsx
+++ src/components/Checkbox/Checkbox.styled.tsx
@@ -1,52 +1,44 @@
-import React from "react";
-import { styled } from "@mui/material/styles";
+import React, { ComponentType } from 'react';
+import { styled } from '@mui/material/styles';
 
-import {
-  Checkbox as MuiCheckbox,
-  FormControlLabel,
-  FormHelperText,
-} from "@mui/material";
+import { Checkbox as MuiCheckbox, FormControlLabel, FormHelperText, CheckboxProps } from '@mui/material';
+
+interface CheckboxStyledProps {
+    textColor: string;
+    fontSize: string | number;
+    helperText: string;
+}
+
+type CheckboxStyledPropsType = CheckboxProps & CheckboxStyledProps;
 
 export const Checkbox = styled(
-  ({
-    required,
-    disabled,
-    labelPlacement,
-    helperText,
-    label = "",
-    fontSize,
-    customColor,
-    muiColor,
-    sx,
-    ...props
-  }) => (
-    <>
-      <FormControlLabel
-        required={required}
-        disabled={disabled}
-        labelPlacement={labelPlacement}
-        sx={{ m: 0, userSelect: "none" }}
-        control={
-          <MuiCheckbox
-            color={muiColor}
-            sx={{
-              ...sx,
-              ...(fontSize && { "& .MuiSvgIcon-root": { fontSize } }),
-              ...(customColor && {
-                color: customColor,
-                "&.Mui-checked": { color: customColor },
-              }),
-            }}
-            {...props}
-          />
-        }
-        label={label}
-      />
-      {helperText && <FormHelperText>{helperText}</FormHelperText>}
-    </>
-  ),
-  {
-    shouldForwardProp: (prop) =>
-      !["textColor", "fontSize", "helperText"].includes(prop),
-  }
-)``;
+    ({ required, disabled, labelPlacement, helperText, label = '', fontSize, customColor, muiColor, sx, ...props }) => (
+        <>
+            <FormControlLabel
+                required={required}
+                disabled={disabled}
+                labelPlacement={labelPlacement}
+                sx={{ m: 0, userSelect: 'none' }}
+                control={
+                    <MuiCheckbox
+                        color={muiColor}
+                        sx={{
+                            ...sx,
+                            ...(fontSize && { '& .MuiSvgIcon-root': { fontSize } }),
+                            ...(customColor && {
+                                color: customColor,
+                                '&.Mui-checked': { color: customColor },
+                            }),
+                        }}
+                        {...props}
+                    />
+                }
+                label={label}
+            />
+            {helperText && <FormHelperText>{helperText}</FormHelperText>}
+        </>
+    ),
+    {
+        shouldForwardProp: (prop) => !['textColor', 'fontSize', 'helperText'].includes(prop),
+    }
+)<CheckboxStyledPropsType>`` as ComponentType<CheckboxStyledPropsType>;
--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -1,78 +1,97 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { Checkbox as MuiCheckbox } from "./Checkbox.styled";
-import { useCustomColor } from "../../utils/helpers";
+import React, { ReactNode } from 'react';
+import PropTypes from 'prop-types';
+import { Checkbox as MuiCheckbox } from './Checkbox.styled';
+import { useCustomColor } from '../../utils/helpers';
 
-const Checkbox = ({
-  label,
-  size,
-  color,
-  textColor: _textColor,
-  checked,
-  onChange,
-  icon,
-  checkedIcon,
-  defaultChecked,
-  required,
-  disabled,
-  labelPlacement,
-  helperText,
-  fontSize,
-  ...props
-}) => {
-  const [customColor, muiColor] = useCustomColor(color);
-  const [textColor] = useCustomColor(_textColor);
+type LabelPlacementType = 'top' | 'start' | 'bottom' | 'end';
+type SizeType = 'small' | 'medium' | 'large';
+interface CheckboxProps {
+    checked?: boolean;
+    checkedIcon?: ReactNode;
+    color?: string;
+    defaultChecked?: boolean;
+    disabled?: boolean;
+    fontSize?: string | number;
+    helperText?: string;
+    icon?: ReactNode;
+    label?: string;
+    labelPlacement?: LabelPlacementType;
+    required?: boolean;
+    size?: SizeType;
 
-  return (
-    <MuiCheckbox
-      label={label}
-      size={size}
-      customColor={muiColor ? undefined : customColor}
-      muiColor={muiColor}
-      textColor={textColor}
-      icon={icon}
-      checkedIcon={checkedIcon}
-      defaultChecked={defaultChecked}
-      required={required}
-      disabled={disabled}
-      checked={checked}
-      onChange={onChange}
-      labelPlacement={labelPlacement}
-      helperText={helperText}
-      fontSize={fontSize}
-      {...props}
-    />
-  );
-};
+    [key: string]: any;
+}
+
+export default function Checkbox({
+    label,
+    size,
+    color,
+    textColor: _textColor,
+    checked,
+    onChange,
+    icon,
+    checkedIcon,
+    defaultChecked,
+    required,
+    disabled,
+    labelPlacement,
+    helperText,
+    fontSize,
+    ...props
+}: CheckboxProps) {
+    const [customColor, muiColor] = useCustomColor(color);
+    const [textColor] = useCustomColor(_textColor);
+
+    return (
+        <MuiCheckbox
+            label={label}
+            size={size}
+            customColor={muiColor ? undefined : customColor}
+            muiColor={muiColor}
+            textColor={textColor}
+            icon={icon}
+            checkedIcon={checkedIcon}
+            defaultChecked={defaultChecked}
+            required={required}
+            disabled={disabled}
+            checked={checked}
+            onChange={onChange}
+            labelPlacement={labelPlacement}
+            helperText={helperText}
+            fontSize={fontSize}
+            {...props}
+        />
+    );
+}
 
 Checkbox.propTypes = {
-  label: PropTypes.string,
-  size: PropTypes.oneOf(["small", "medium", "large"]),
-  color: PropTypes.string,
-  checked: PropTypes.bool,
-  icon: PropTypes.node,
-  checkedIcon: PropTypes.node,
-  defaultChecked: PropTypes.bool,
-  required: PropTypes.bool,
-  disabled: PropTypes.bool,
-  labelPlacement: PropTypes.oneOf(["top", "start", "bottom", "end"]),
-  helperText: PropTypes.string,
-  fontSize: PropTypes.string,
+    checked: PropTypes.bool,
+    checkedIcon: PropTypes.node,
+    color: PropTypes.string,
+    defaultChecked: PropTypes.bool,
+    disabled: PropTypes.bool,
+    fontSize: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    helperText: PropTypes.string,
+    icon: PropTypes.node,
+    label: PropTypes.string,
+    labelPlacement: PropTypes.oneOf(['top', 'start', 'bottom', 'end']),
+    required: PropTypes.bool,
+    size: PropTypes.oneOf(['small', 'medium', 'large']),
 };
 
 Checkbox.defaultProps = {
-  label: undefined,
-  size: "medium",
-  color: undefined,
-  checked: undefined,
-  icon: undefined,
-  checkedIcon: undefined,
-  defaultChecked: undefined,
-  required: false,
-  disabled: false,
-  labelPlacement: undefined,
-  helperText: undefined,
-  fontSize: undefined,
+    checked: undefined,
+    checkedIcon: undefined,
+    color: undefined,
+    defaultChecked: undefined,
+    disabled: false,
+    fontSize: undefined,
+    helperText: undefined,
+    icon: undefined,
+    label: undefined,
+    labelPlacement: undefined,
+    required: false,
+    size: 'medium',
 };
 
 export default Checkbox;
