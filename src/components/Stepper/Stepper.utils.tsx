2800cbab11221bad860cf8636dd55debe718d281
afekTheMiniLearner <afeksa2000@gmail.com>
2023-07-06 02:04:05 +0300

    added type for the index in stepper utils file

--- src/components/Stepper/Stepper.utils.tsx
+++ src/components/Stepper/Stepper.utils.tsx
@@ -1,24 +1,24 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 export const useSimpleStepper = () => {
-  const [activeStep, setActiveStep] = useState(0);
-  const [skipSteps, setSkipSteps] = useState([]);
+    const [activeStep, setActiveStep] = useState(0);
+    const [skipSteps, setSkipSteps] = useState([]);
 
-  return {
-    stepIndex: activeStep,
-    onReset: () => {
-      setSkipSteps([]);
-      setActiveStep(0);
-    },
-    onNext: (index) => {
-      setSkipSteps((a) => a.filter((i) => i !== index));
-      setActiveStep(index + 1);
-    },
-    onBack: (index) => setActiveStep(index - 1),
-    onSkip: (index) => {
-      setSkipSteps((a) => (a.includes(index) ? a : [...a, index]));
-      setActiveStep(index + 1);
-    },
-    stepsIndexSkipped: skipSteps,
-  };
+    return {
+        stepIndex: activeStep,
+        onReset: () => {
+            setSkipSteps([]);
+            setActiveStep(0);
+        },
+        onNext: (index: number) => {
+            setSkipSteps((a) => a.filter((i) => i !== index));
+            setActiveStep(index + 1);
+        },
+        onBack: (index: number) => setActiveStep(index - 1),
+        onSkip: (index: number) => {
+            setSkipSteps((a) => (a.includes(index) ? a : [...a, index]));
+            setActiveStep(index + 1);
+        },
+        stepsIndexSkipped: skipSteps,
+    };
 };
