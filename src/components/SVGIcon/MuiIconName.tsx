48468026b5ae9d23cefd55e3aa76bb7d6652f9a1
hdriel <hdriel@gmail.com>
2023-07-07 00:14:22 +0300

    update SVGIcon

--- src/components/SVGIcon/MuiIconName.tsx
+++ src/components/SVGIcon/MuiIconName.tsx
@@ -1,9 +1,25 @@
 import { createElement } from 'react';
-import PropTypes from 'prop-types';
+import type { PropsWithChildren } from 'react';
+// import PropTypes from 'prop-types';
 import * as MUIIcon from '@mui/icons-material';
 import { numberToPx } from '../../utils/helpers';
 
-export default function MuiIconName({ name, color, width, height, children, ...props }) {
+interface MuiIconNameProps {
+    name: string;
+    color: string;
+    width: string | number;
+    height: string | number;
+}
+type MuiIconNamePropsType = PropsWithChildren<MuiIconNameProps>;
+
+export default function MuiIconName({
+    name,
+    color,
+    width,
+    height,
+    children,
+    ...props
+}: MuiIconNamePropsType): React.ReactElement {
     const Icon = name && MUIIcon[name];
 
     return Icon
@@ -14,12 +30,12 @@ export default function MuiIconName({ name, color, width, height, children, ...p
         : children;
 }
 
-MuiIconName.propTypes = {
-    name: PropTypes.string,
-    color: PropTypes.string,
-    width: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-    height: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-};
+// MuiIconName.propTypes = {
+//     name: PropTypes.string,
+//     color: PropTypes.string,
+//     width: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+//     height: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+// };
 
 MuiIconName.defaultProps = {
     name: undefined,
