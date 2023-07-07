520d9ed50294ec6ec7a472bd68ff3d3bb392abe3
afekTheMiniLearner <afeksa2000@gmail.com>
2023-07-08 00:53:37 +0300

    fixed all errors in Alert

--- src/components/Alert/Alert.styled.tsx
+++ src/components/Alert/Alert.styled.tsx
@@ -1,11 +1,18 @@
 import type { ComponentType } from 'react';
-import { Alert as MuiAlert, AlertTitle as MuiAlertTitle, alpha, AlertProps, AlertTitleProps } from '@mui/material';
 import { styled, css } from '@mui/material/styles';
+import { Alert as MuiAlert, AlertTitle as MuiAlertTitle, alpha } from '@mui/material';
+import type { AlertProps, AlertTitleProps } from '@mui/material';
+
 import { numberToPx } from '../../utils/helpers';
 
+interface AlertStyledProps {
+    customColor?: string;
+    width?: string | number;
+}
+type AlertStyledPropsType = AlertStyledProps & AlertProps;
 export const Alert = styled(MuiAlert, {
     shouldForwardProp: (propName) => !['customColor'].includes(propName as string),
-})`
+})<AlertStyledPropsType>`
     &.MuiAlert-root {
         min-width: 200px;
     }
