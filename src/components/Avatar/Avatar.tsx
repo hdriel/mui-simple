cdf309f3bf6c4c63a459d10da151cfd811e92080
afekTheMiniLearner <afeksa2000@gmail.com>
2023-07-08 02:29:03 +0300

    fixed all Avatar component TS errors

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -1,28 +1,46 @@
 import React, { useState } from 'react';
-import PropTypes from 'prop-types';
+import type { PropsWithChildren, ReactNode } from 'react';
+//	import PropTypes from 'prop-types';
 import { Avatar as MuiAvatar } from '@mui/material';
+
 import { getCapitalLetters, stringToColor, useCustomColor } from '../../utils/helpers';
 import Tooltip from '../Tooltip/Tooltip';
-import { TOOLTIP_PLACEMENTS } from '../Tooltip/Tooltip.consts';
+//	import { TOOLTIP_PLACEMENTS } from '../Tooltip/Tooltip.consts';
 
-export default function Avatar({
-    username,
-    image,
-    size,
-    icon,
-    variant,
-    color,
-    fallbackImage,
-    onClick,
-    showTooltip,
-    tooltipPlacement,
-    ...props
-}) {
+type Variant = 'circular' | 'rounded' | 'square';
+type tooltipPlacement = 'top' | 'right' | 'bottom' | 'left';
+interface AvatarProps {
+    color?: string;
+    fallbackImage?: string;
+    icon?: ReactNode;
+    image?: string;
+    onClick?: () => void;
+    showTooltip?: boolean;
+    size?: string;
+    tooltipPlacement?: tooltipPlacement;
+    username?: string;
+    variant?: Variant;
+    [key: string]: any;
+}
+export default function Avatar(props: PropsWithChildren<AvatarProps>): ReactNode {
+    const {
+        username,
+        image,
+        size,
+        icon,
+        variant,
+        color,
+        fallbackImage,
+        onClick,
+        showTooltip,
+        tooltipPlacement,
+        ...rest
+    } = props;
     const [url, setUrl] = useState(image);
     const [fallbackSet, setFallbackSet] = useState(false);
     const [customColor] = useCustomColor(color);
 
-    const errorHandler = () => {
+    const errorHandler = (): void => {
         if (fallbackSet) {
             setUrl(undefined);
         } else {
@@ -42,7 +60,7 @@ export default function Avatar({
                 variant={variant}
                 imgProps={{ onError: errorHandler }}
                 onClick={onClick}
-                {...props}
+                {...rest}
             >
                 {!url && (icon ?? getCapitalLetters(username))}
             </MuiAvatar>
@@ -50,26 +68,27 @@ export default function Avatar({
     );
 }
 
-Avatar.propTypes = {
-    username: PropTypes.string,
-    image: PropTypes.string,
-    size: PropTypes.string,
-    icon: PropTypes.node,
-    variant: PropTypes.oneOf(['circular', 'rounded', 'square']),
-    fallbackImage: PropTypes.string,
-    onClick: PropTypes.func,
-    showTooltip: PropTypes.bool,
-    tooltipPlacement: PropTypes.oneOf(TOOLTIP_PLACEMENTS),
-};
+//	Avatar.propTypes = {
+//		fallbackImage: PropTypes.string,
+//		icon: PropTypes.node,
+//		image: PropTypes.string,
+//		onClick: PropTypes.func,
+//		showTooltip: PropTypes.bool,
+//		size: PropTypes.string,
+//		tooltipPlacement: PropTypes.oneOf(TOOLTIP_PLACEMENTS),
+//		username: PropTypes.string,
+//		variant: PropTypes.oneOf(['circular', 'rounded', 'square']),
+//	};
 
 Avatar.defaultProps = {
-    username: undefined,
-    image: undefined,
-    size: undefined,
-    icon: undefined,
-    variant: 'circular',
+    color: undefined,
     fallbackImage: undefined,
+    icon: undefined,
+    image: undefined,
     onClick: undefined,
     showTooltip: false,
+    size: undefined,
     tooltipPlacement: undefined,
+    username: undefined,
+    variant: 'circular',
 };
