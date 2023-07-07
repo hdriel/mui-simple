93077d4b8821cb8507d8d12bf9782078b882f552
afekTheMiniLearner <afeksa2000@gmail.com>
2023-07-08 02:58:03 +0300

    fixed almost all ButtomNav component TS errors

--- src/components/BottomNavigation/BottomNavigation.styled.tsx
+++ src/components/BottomNavigation/BottomNavigation.styled.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import { get } from 'lodash-es';
+import { styled } from '@mui/material/styles';
 import {
     BottomNavigation as MuiBottomNavigation,
     BottomNavigationAction as MuiBottomNavigationAction,
 } from '@mui/material';
-import { styled } from '@mui/material/styles';
+import type { BottomNavigationProps } from '@mui/material';
+
 import Paper from '../Paper/Paper';
 
+type Position = 'absolute' | 'fixed';
+interface BottomNavigationStyledProps {
+    fixedToBottom?: boolean;
+    customColor?: string;
+    elevation?: number;
+    fixedToTop?: boolean;
+    muiColor?: string;
+    width?: number | string;
+    position?: Position;
+}
+type BottomNavigationStyledPropsType = BottomNavigationProps & BottomNavigationStyledProps;
 export const BottomNavigation = styled(
     ({ width, elevation, fixedToTop, fixedToBottom, position, ...props }) => (
         <Paper
@@ -28,7 +41,7 @@ export const BottomNavigation = styled(
     {
         shouldForwardProp: (propName) => !['muiColor', 'customColor'].includes(propName as string),
     }
-)`
+)<BottomNavigationStyledPropsType>`
     & .MuiBottomNavigationAction-root.Mui-selected {
         color: ${(props) =>
             get(props, `theme.palette.${props.muiColor}.main`) ??
