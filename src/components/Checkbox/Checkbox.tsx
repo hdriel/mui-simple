d65105411d589db508c92e25fb8fe7d472063965
afekTheMiniLearner <afeksa2000@gmail.com>
2023-07-08 14:25:01 +0300

    fixed almost all Checkbox folder content TS and ESLINT errors

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -1,5 +1,6 @@
-import React, { ReactNode } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
+import type { ReactNode, PropsWithChildren, ChangeEvent } from 'react';
+//	import PropTypes from 'prop-types';
 import { Checkbox as MuiCheckbox } from './Checkbox.styled';
 import { useCustomColor } from '../../utils/helpers';
 
@@ -13,38 +14,41 @@ interface CheckboxProps {
     disabled?: boolean;
     fontSize?: string | number;
     helperText?: string;
+    textColor?: string;
+    // Todo: assert if this type fit the onChange
+    onChange?: (event: ChangeEvent<HTMLInputElement>, checked: boolean) => void;
     icon?: ReactNode;
     label?: string;
     labelPlacement?: LabelPlacementType;
     required?: boolean;
     size?: SizeType;
-
     [key: string]: any;
 }
-
-export default function Checkbox({
-    label,
-    size,
-    color,
-    textColor: _textColor,
-    checked,
-    onChange,
-    icon,
-    checkedIcon,
-    defaultChecked,
-    required,
-    disabled,
-    labelPlacement,
-    helperText,
-    fontSize,
-    ...props
-}: CheckboxProps) {
+export default function Checkbox(props: PropsWithChildren<CheckboxProps>): ReactNode {
+    const {
+        label,
+        size,
+        color,
+        textColor: _textColor,
+        checked,
+        onChange,
+        icon,
+        checkedIcon,
+        defaultChecked,
+        required,
+        disabled,
+        labelPlacement,
+        helperText,
+        fontSize,
+        ...rest
+    } = props;
     const [customColor, muiColor] = useCustomColor(color);
     const [textColor] = useCustomColor(_textColor);
 
     return (
         <MuiCheckbox
             label={label}
+            // Todo: is it ok to remove the 'large' from the Size type
             size={size}
             customColor={muiColor ? undefined : customColor}
             muiColor={muiColor}
@@ -59,25 +63,25 @@ export default function Checkbox({
             labelPlacement={labelPlacement}
             helperText={helperText}
             fontSize={fontSize}
-            {...props}
+            {...rest}
         />
     );
 }
 
-Checkbox.propTypes = {
-    checked: PropTypes.bool,
-    checkedIcon: PropTypes.node,
-    color: PropTypes.string,
-    defaultChecked: PropTypes.bool,
-    disabled: PropTypes.bool,
-    fontSize: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-    helperText: PropTypes.string,
-    icon: PropTypes.node,
-    label: PropTypes.string,
-    labelPlacement: PropTypes.oneOf(['top', 'start', 'bottom', 'end']),
-    required: PropTypes.bool,
-    size: PropTypes.oneOf(['small', 'medium', 'large']),
-};
+//	Checkbox.propTypes = {
+//    checked: PropTypes.bool,
+//    checkedIcon: PropTypes.node,
+//    color: PropTypes.string,
+//    defaultChecked: PropTypes.bool,
+//    disabled: PropTypes.bool,
+//    fontSize: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+//    helperText: PropTypes.string,
+//    icon: PropTypes.node,
+//    label: PropTypes.string,
+//    labelPlacement: PropTypes.oneOf(['top', 'start', 'bottom', 'end']),
+//    required: PropTypes.bool,
+//    size: PropTypes.oneOf(['small', 'medium', 'large']),
+//	};
 
 Checkbox.defaultProps = {
     checked: undefined,
