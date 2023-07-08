2df821099cf178907cd982869d5e1e9858e408a1
afekTheMiniLearner <afeksa2000@gmail.com>
2023-07-08 14:58:57 +0300

    fixed almost all Divider folder content TS and ESLINT errors

--- src/components/Divider/Divider.tsx
+++ src/components/Divider/Divider.tsx
@@ -1,28 +1,31 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { isValidElement, cloneElement } from 'react';
+import type { ReactNode, PropsWithChildren } from 'react';
+//	import PropTypes from 'prop-types';
+
 import { Divider as MuiDivider } from './Divider.styled';
 import { useCustomColor } from '../../utils/helpers';
 
-export default function Divider({
-    orientation,
-    light,
-    flexItem,
-    textAlign,
-    variant,
-    component,
-    label,
-    thickness,
-    color,
-    children,
-    ...props
-}) {
+interface DividerProps {
+    orientation?: 'horizontal' | 'vertical';
+    light?: boolean;
+    flexItem?: boolean;
+    textAlign?: 'center' | 'left' | 'right';
+    variant?: 'fullWidth' | 'inset' | 'middle';
+    component?: ReactNode;
+    label?: string | ReactNode;
+    thickness?: number;
+    color?: string;
+}
+export default function Divider(props: PropsWithChildren<DividerProps>): ReactNode {
+    const { orientation, light, flexItem, textAlign, variant, component, label, thickness, color, children, ...rest } =
+        props;
     const [customColor] = useCustomColor(color);
 
     const content = [label]
         .concat(children ?? [])
         .map((child, index) => {
-            return React.isValidElement(child)
-                ? React.cloneElement(child, {
+            return isValidElement(child)
+                ? cloneElement(child, {
                       key: `DC${index}`,
                       ...{ color: child.props.color ?? color },
                   })
@@ -37,27 +40,28 @@ export default function Divider({
             flexItem={flexItem ?? (orientation === 'vertical' ? true : undefined)}
             textAlign={textAlign}
             variant={variant}
+            // Todo: find correct type for this instead of ReactNode
             component={component}
             thickness={thickness}
             customColor={customColor}
-            {...props}
+            {...rest}
         >
             {content.length ? content : undefined}
         </MuiDivider>
     );
 }
 
-Divider.propTypes = {
-    orientation: PropTypes.oneOf(['horizontal', 'vertical']),
-    light: PropTypes.bool,
-    flexItem: PropTypes.bool,
-    textAlign: PropTypes.oneOf(['center', 'left', 'right']),
-    variant: PropTypes.oneOf(['fullWidth', 'inset', 'middle']),
-    component: PropTypes.string,
-    label: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
-    thickness: PropTypes.number,
-    color: PropTypes.string,
-};
+//	Divider.propTypes = {
+//    orientation: PropTypes.oneOf(['horizontal', 'vertical']),
+//    light: PropTypes.bool,
+//    flexItem: PropTypes.bool,
+//    textAlign: PropTypes.oneOf(['center', 'left', 'right']),
+//    variant: PropTypes.oneOf(['fullWidth', 'inset', 'middle']),
+//    component: PropTypes.string,
+//    label: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+//    thickness: PropTypes.number,
+//    color: PropTypes.string,
+//	};
 
 Divider.defaultProps = {
     orientation: undefined,
