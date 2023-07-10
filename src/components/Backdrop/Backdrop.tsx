9c8a5f096967ba6a03029a440046355992bfc22a
Hadriel Benjo <hdriel@gmail.com>
2023-07-10 15:54:50 +0300

    update types

--- src/components/Backdrop/Backdrop.tsx
+++ src/components/Backdrop/Backdrop.tsx
@@ -9,7 +9,7 @@ interface BackdropProps {
     invisible?: boolean;
     [key: string]: any;
 }
-const Backdrop: React.FC<PropsWithChildren<BackdropProps>> = function (props): ReactNode {
+const Backdrop: React.FC<PropsWithChildren<BackdropProps>> = function (props): React.ReactElement {
     const { open, color, onClick, invisible, ...rest } = props;
 
     return <MuiBackdrop color={color} open={open} onClick={onClick} invisible={invisible} {...rest} />;
