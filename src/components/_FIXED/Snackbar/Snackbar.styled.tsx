ab329b0883cc2dd52c34df0343c4fc899c42c2b8
Hadriel Benjo <hdriel@gmail.com>
2023-07-16 16:20:59 +0300

    update Snackbar

--- src/components/_FIXED/Snackbar/Snackbar.styled.tsx
+++ src/components/_FIXED/Snackbar/Snackbar.styled.tsx
@@ -1,12 +1,17 @@
 import { Snackbar as MuiSnackbar } from '@mui/material';
-import { styled } from '@mui/material/styles';
+import { styled, css } from '@mui/material/styles';
 
 export { Fade, Grow, Slide } from '@mui/material';
 
 export const Snackbar = styled(MuiSnackbar, {
     shouldForwardProp: (propName) => !['fullWidth'].includes(propName as string),
 })`
-    width: 100%;
-    box-sizing: border-box;
-    padding-right: 3em;
+    ${(props) =>
+        props.fullWidth
+            ? css`
+                  width: 100%;
+                  box-sizing: border-box;
+                  padding-right: 3em;
+              `
+            : css``}
 `;
