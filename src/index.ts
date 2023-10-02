d9dd63c692e8a14b40cd721608447a85c6b52ae8
hdriel <hdriel@gmail.com>
2023-10-02 19:31:48 +0300

    update input date, time, date-time

--- src/index.ts
+++ src/index.ts
@@ -60,7 +60,10 @@ export { default as InputColor } from './components/_FIXED/TextField/InputColor'
 export type { InputColorProps } from './components/_FIXED/TextField/InputColor';
 
 export { default as InputDate } from './components/_FIXED/TextField/InputDate';
+export type { InputDateProps } from './components/_FIXED/TextField/InputDate';
+
 export { default as InputDateTime } from './components/_FIXED/TextField/InputDateTime';
+export type { InputDateTimeProps } from './components/_FIXED/TextField/InputDateTime';
 
 export { default as InputEmail } from './components/_FIXED/TextField/InputEmail';
 export type { InputEmailProps } from './components/_FIXED/TextField/InputEmail';
@@ -86,6 +89,7 @@ export { default as InputText } from './components/_FIXED/TextField/InputText';
 export type { InputTextProps } from './components/_FIXED/TextField/InputText';
 
 export { default as InputTime } from './components/_FIXED/TextField/InputTime';
+export type { InputTimeProps } from './components/_FIXED/TextField/InputTime';
 
 export { default as InputSelect } from './components/_FIXED/Select/InputSelect';
 export type { InputSelectOptions, InputSelectProps } from './components/_FIXED/Select/InputSelect';
