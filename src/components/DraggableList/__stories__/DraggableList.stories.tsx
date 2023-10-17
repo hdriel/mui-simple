048ba0e1b844c92fa5970f926ef24b6aa9545aaf
Hadriel Benjo <hdriel@gmail.com>
2023-10-17 17:17:08 +0300

    update DragAndDrop stories

--- src/components/DraggableList/__stories__/DraggableList.stories.tsx
+++ src/components/DraggableList/__stories__/DraggableList.stories.tsx
@@ -1,14 +1,30 @@
 import React, { useState } from 'react';
+import configureMockStore from 'redux-mock-store';
+import { shallow } from 'enzyme';
+import { Provider } from 'react-redux';
 import type { Meta, StoryObj } from '@storybook/react';
 
 import DraggableList from '../DraggableList';
 import Avatar from '../../_FIXED/Avatar/Avatar';
 import Button from '../../_FIXED/Button/Button';
 
+const mockStore = configureMockStore();
+const store = mockStore({});
+
+// need redux for these stories
+// https://storybook.js.org/addons/@dreamworld/addon-redux
 const meta: Meta<typeof DraggableList> = {
     title: 'Wrappers/DraggableList',
     component: DraggableList,
     tags: ['autodocs'],
+    decorators: [
+        (Story) =>
+            shallow(
+                <Provider store={store}>
+                    <Story />
+                </Provider>
+            ),
+    ],
 };
 
 export default meta;
@@ -33,6 +49,7 @@ export const AvatarsColumn: Story = {
         fieldId: 'image',
         flexDirection: 'column',
         flexGap: '10px',
+        droppableClassName: 'images',
         renderValue: (props) => <Avatar {...props} />,
     },
     render: (args) => {
@@ -55,6 +72,7 @@ export const AvatarsRow: Story = {
         fieldId: 'image',
         flexDirection: 'row',
         flexGap: '10px',
+        droppableClassName: 'images',
         renderValue: (props) => <Avatar {...props} />,
     },
     render: (args) => {
@@ -75,6 +93,7 @@ export const ButtonsColumn: Story = {
         fieldId: 'image',
         flexDirection: 'column',
         flexGap: '10px',
+        droppableClassName: 'images',
         renderValue: (props) => <Button {...props} />,
     },
     render: (args) => {
@@ -95,6 +114,7 @@ export const ButtonsRow: Story = {
         fieldId: 'image',
         flexDirection: 'row',
         flexGap: '10px',
+        droppableClassName: 'images',
         renderValue: (props) => <Button {...props} />,
     },
     render: (args) => {
