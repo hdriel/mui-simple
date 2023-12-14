0b4141309be3a397174f8650705987a6bd99c2e6
Hadriel Benjo <hdriel@gmail.com>
2023-12-14 16:55:48 +0200

    update InputTime component

--- src/components/_FIXED/TextField/InputTime.tsx
+++ src/components/_FIXED/TextField/InputTime.tsx
@@ -1,132 +1,136 @@
 import React from 'react';
+// https://mui.com/x/react-date-pickers/adapters-locale/
+import { TimePicker } from '@mui/x-date-pickers/TimePicker';
+import { MobileTimePicker } from '@mui/x-date-pickers/MobileTimePicker';
+import { DesktopTimePicker } from '@mui/x-date-pickers/DesktopTimePicker';
+import { StaticTimePicker } from '@mui/x-date-pickers/StaticTimePicker';
+import { DayCalendarSkeleton } from '@mui/x-date-pickers/DayCalendarSkeleton';
+import { TimeField } from '@mui/x-date-pickers';
+
+// Decide to use dayjs and not date-fns for supporting timezone
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import Input from './TextField';
+
 import type { InputTimeProps } from '../../decs';
+import { onChangeEventDateHandler } from './InputDate.hooks';
+import LocalizationProvider from './LocalizationProvider';
 
 const InputTime: React.FC<InputTimeProps> = ({
     value,
     valueType: _valueType,
     onChange,
+    minTime,
+    maxTime,
+    readOnly,
+    useLocalizationProvider,
+    locale,
+    adapterLocale,
+    inputProps,
+    pickerVariant,
     InputLabelProps,
+    clearable,
+    onClearClick,
+    name,
+    label,
+    required,
+    className,
+    direction,
+    endCmp,
+    endCmpExternal,
+    startCmpExternal,
+    startCmp,
+    variant,
+    width,
+    dateIcon,
+    format,
+    displayWeekNumber,
+    showDaysOutsideCurrentMonth,
+    loading,
+    openTo,
+    helperText,
+    timezone,
     ...props
 }): React.ReactElement => {
-    let valueType = 'string';
-    const date = new Date(value);
+    const slotProps = {
+        slots: {
+            ...props.slots,
+        },
+        slotProps: {
+            ...props.slotProps,
+            textField: {
+                ...props.slotProps?.textField,
+                variant,
+                required,
+                InputProps: {
+                    ...props.slotProps?.inputProps,
+                    name,
+                    className,
+                },
+                helperText,
+                clearable: true,
+                onClear: () => onClearClick?.(),
+            },
+            tabs: {
+                hidden: false,
+                ...props.slotProps?.tabs,
+                dateIcon,
+            },
+        },
+    };
 
-    const totalSecondsInOneDay = 60 * 60 * 24;
-    const totalMinutesInOneDay = 60 * 24;
+    const pickerProps = {
+        ...props,
+        value,
+        label,
+        minTime,
+        maxTime,
+        format,
+        onChange: (e) => {
+            const event = onChangeEventDateHandler({ event: e, valueType, resetTime: true });
+            return onChange?.(event);
+        },
+        ...(width && { sx: { width } }),
+    };
 
-    if (value === undefined || value === null) {
-        value = undefined;
-        valueType = undefined;
-    }
+    const dateCmp = readOnly ? (
+        <TimeField value={value} readOnly />
+    ) : (
+        {
+            mobile: <MobileTimePicker {...pickerProps} {...slotProps} />,
+            desktop: <DesktopTimePicker {...pickerProps} {...slotProps} />,
+            static: <StaticTimePicker {...pickerProps} />,
+        }[pickerVariant] ?? <TimePicker {...pickerProps} {...slotProps} />
+    );
 
-    // minutes time
-    else if (typeof value === 'number' && _valueType === 'minutes' && value < totalMinutesInOneDay) {
-        const time = value * 60;
-        value = timeNumberToHHMM(time, false);
-        valueType = 'seconds';
-    }
-
-    // seconds time
-    else if (typeof value === 'number' && value < totalSecondsInOneDay) {
-        const time = value;
-        value = timeNumberToHHMM(time, false);
-        valueType = 'seconds';
-    }
-
-    // milliseconds time
-    else if (typeof value === 'number' && value < 86400 * 1000) {
-        value = timeNumberToHHMM(value, true);
-        valueType = 'milliseconds';
-    }
-
-    // timestamp time value
-    else if (typeof value === 'number' && date.getTime() > 0) {
-        value = 'HH:MM'
-            .replace('HH', `${date.getHours()}`.padStart(2, '0'))
-            .replace('MM', `${date.getMinutes()}`.padStart(2, '0'));
-        valueType = 'timestamp';
-    }
-
-    // date time value
-    else if (value instanceof Date) {
-        value = 'HH:MM'
-            .replace('HH', `${date.getHours()}`.padStart(2, '0'))
-            .replace('MM', `${date.getMinutes()}`.padStart(2, '0'));
-        valueType = 'date';
-    }
-
-    if (['seconds', 'minutes', 'milliseconds', 'timestamp', 'date', 'string'].includes(_valueType)) {
-        valueType = _valueType;
-    } else if (_valueType !== undefined) {
-        console.warn(
-            `invalid valueType value supplied for InputTime component, must be one of: ['seconds', 'minutes', 'milliseconds', 'timestamp', 'date', 'string'], got: ${_valueType}`
-        );
-    }
-
-    return (
-        <Input
-            {...props}
-            value={value}
-            InputLabelProps={{ shrink: true, ...InputLabelProps }}
-            onChange={(e) => {
-                const event = { ...e, target: { ...e.target } };
-                const currDate = new Date(date).getTime();
-                const ms = HHMMToTime(event.target.value, true);
-
-                switch (valueType) {
-                    case 'milliseconds':
-                        event.target.value = ms;
-                        break;
-                    case 'minutes':
-                        event.target.value = HHMMToTime(event.target.value, false) / 60;
-                        break;
-                    case 'seconds':
-                        event.target.value = HHMMToTime(event.target.value, false);
-                        break;
-                    case 'timestamp':
-                        event.target.value = currDate + ms;
-                        break;
-                    case 'date':
-                        event.target.value = new Date(currDate + ms);
-                        break;
-                    case 'string':
-                        break;
-                    default:
-                        break;
-                }
-                return onChange?.(event);
-            }}
-            type="time"
-        />
+    return useLocalizationProvider ? (
+        <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale={adapterLocale} locale={locale}>
+            {dateCmp}
+        </LocalizationProvider>
+    ) : (
+        dateCmp
     );
 };
 
-function timeNumberToHHMM(time: number, inMilliseconds: boolean): string {
-    // Convert milliseconds to seconds
-    const totalSeconds = inMilliseconds ? Math.floor(time / 1000) : time;
-
-    // Calculate hours and minutes
-    const hours = Math.floor(totalSeconds / 3600);
-    const minutes = Math.floor((totalSeconds % 3600) / 60);
-
-    // Format the result as HH:MM
-    const formattedTime = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
-
-    return formattedTime;
-}
-
-function HHMMToTime(time, inMilliseconds: boolean): number {
-    // Split the time string into hours and minutes
-    const [hours, minutes] = time.split(':').map(Number);
-
-    // Calculate the total milliseconds
-    const totalTime = (hours * 3600 + minutes * 60) * (inMilliseconds ? 1000 : 1);
-
-    return totalTime;
-}
-
-InputTime.defaultProps = Input.defaultProps;
+InputTime.defaultProps = {
+    ...Input.defaultProps,
+    useLocalizationProvider: false,
+    timezone: 'Asia/Jerusalem',
+    locale: 'he',
+    adapterLocale: AdapterDayjs,
+    pickerVariant: 'desktop',
+    // views: ['year', 'month', 'day'],
+    orientation: 'portrait',
+    clearable: true,
+    onClearClick: undefined,
+    displayWeekNumber: false,
+    showDaysOutsideCurrentMonth: true,
+    openTo: undefined,
+    loading: false,
+    dateIcon: undefined,
+    format: 'DD-MM-YYYY HH:mm',
+    ampm: false,
+    ampmInClock: false,
+};
 
 export type { InputTimeProps } from '../../decs';
 export default InputTime;
--- src/components/_FIXED/TextField/stories/InputTime.stories.tsx
+++ src/components/_FIXED/TextField/stories/InputTime.stories.tsx
@@ -1,17 +1,22 @@
-// @ts-expect-error
-import React, { useState } from 'react';
-// @ts-expect-error
-import type { ReactElement } from 'react';
+import React from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 import { Stack } from '@mui/material';
-import { Send as SendIcon } from '@mui/icons-material';
+import dayjs from 'dayjs';
 
 import InputTime from '../InputTime';
+import LocalizationProvider from '../LocalizationProvider';
 
 const meta: Meta<typeof InputTime> = {
     title: 'Inputs/Inputs/InputTime',
     component: InputTime,
     tags: ['autodocs'],
+    decorators: [
+        (Story) => (
+            <LocalizationProvider>
+                <Story />
+            </LocalizationProvider>
+        ),
+    ],
 };
 
 export default meta;
@@ -19,389 +24,64 @@ export default meta;
 type Story = StoryObj<typeof InputTime>;
 
 export const Default: Story = {
-    args: {},
-};
-
-export const ValueString: Story = {
     args: {
-        label: 'Time',
-        value: '20:00',
-    },
-    render: (args) => {
-        // eslint-disable-next-line react-hooks/rules-of-hooks
-        const [value, onChange] = useState(args.value);
-        return (
-            <InputTime
-                {...args}
-                value={value}
-                onChange={(e) => {
-                    const value = e.target.value;
-                    console.log(typeof value, value);
-                    onChange(value);
-                }}
-            />
-        );
+        variant: 'standard',
     },
 };
 
-export const ValueTimestamp: Story = {
+export const Variant: Story = {
     args: {
-        label: 'Time',
-        value: Date.now(),
-    },
-    render: (args) => {
-        // eslint-disable-next-line react-hooks/rules-of-hooks
-        const [value, onChange] = useState(args.value);
-        return (
-            <InputTime
-                {...args}
-                value={value}
-                onChange={(e) => {
-                    const value = e.target.value;
-                    console.log(typeof value, value);
-                    onChange(value);
-                }}
-            />
-        );
-    },
-};
-
-export const ValueMinutes: Story = {
-    args: {
-        label: 'Time',
-        value: 90,
-        valueType: 'minutes',
-    },
-    render: (args) => {
-        // eslint-disable-next-line react-hooks/rules-of-hooks
-        const [value, onChange] = useState(args.value);
-        return (
-            <InputTime
-                {...args}
-                value={value}
-                onChange={(e) => {
-                    const value = e.target.value;
-                    console.log(typeof value, value);
-                    onChange(value);
-                }}
-            />
-        );
-    },
-};
-
-export const ValueDate: Story = {
-    args: {
-        label: 'Time',
-        value: new Date(),
-    },
-    render: (args) => {
-        // eslint-disable-next-line react-hooks/rules-of-hooks
-        const [value, onChange] = useState(args.value);
-        return (
-            <InputTime
-                {...args}
-                value={value}
-                onChange={(e) => {
-                    const value = e.target.value;
-                    console.log(typeof value, value);
-                    onChange(value);
-                }}
-            />
-        );
-    },
-};
-
-export const ValueMilliseconds: Story = {
-    args: {
-        label: 'Time',
-        value: 190 * 1000,
-    },
-    render: (args) => {
-        // eslint-disable-next-line react-hooks/rules-of-hooks
-        const [value, onChange] = useState(args.value);
-        return (
-            <InputTime
-                {...args}
-                value={value}
-                onChange={(e) => {
-                    const value = e.target.value;
-                    console.log(typeof value, value);
-                    onChange(value);
-                }}
-            />
-        );
-    },
-};
-
-export const ValueSeconds: Story = {
-    args: {
-        label: 'Time in Seconds',
-        value: 190,
-    },
-    render: (args) => {
-        // eslint-disable-next-line react-hooks/rules-of-hooks
-        const [value, onChange] = useState(args.value);
-        return (
-            <InputTime
-                {...args}
-                value={value}
-                onChange={(e) => {
-                    const value = e.target.value;
-                    console.log(typeof value, value);
-                    onChange(value);
-                }}
-            />
-        );
-    },
-};
-
-export const OnChange: Story = {
-    args: {
-        label: 'Time',
-        value: new Date(),
-    },
-    render: (args) => {
-        // eslint-disable-next-line react-hooks/rules-of-hooks
-        const [value, onChange] = useState(args.value);
-        return (
-            <InputTime
-                {...args}
-                value={value}
-                onChange={(e) => {
-                    onChange(e.target.value);
-                }}
-            />
-        );
-    },
-};
-
-export const TextAlign: Story = {
-    args: {
-        label: 'Text Align',
-        textAlign: 'center',
-        value: Date.now(),
-    },
-};
-
-export const Direction: Story = {
-    args: {
-        label: 'RTL Direction',
-        direction: 'rtl',
-        value: Date.now(),
-    },
-};
-
-export const LetterSpacing: Story = {
-    args: {
-        label: 'Letter Spacing',
-        letterSpacing: '5px',
-        value: Date.now(),
-    },
-};
-
-export const AlignActions: Story = {
-    args: {
-        alignActions: 'center',
-        startCmp: 'Email',
-        endCmp: 'Fingerprint',
-        label: 'Align Actions',
-        value: Date.now(),
-    },
-};
-
-export const AlignActionsExternal: Story = {
-    args: {
-        alignActions: 'flex-start',
-        startCmpExternal: 'Email',
-        endCmpExternal: 'Fingerprint',
-        label: 'Align Actions External',
-        value: Date.now(),
-    },
-};
-
-export const AutoComplete: Story = {
-    args: {
-        autoComplete: 'email',
-        name: 'email',
-        label: 'Auto Complete',
-    },
-};
-
-export const CmpSpacing: Story = {
-    args: {
-        alignActions: 'flex-start',
-        startCmp: 'Email',
-        endCmp: 'Fingerprint',
-        cmpSpacing: 6,
-        label: 'Cmp Spacing',
-        value: Date.now(),
-    },
-};
-
-export const ColorActive: Story = {
-    args: {
-        colorActive: 'warning',
-        label: 'Color Active',
-        value: Date.now(),
-    },
-};
-
-export const ColorLabel: Story = {
-    args: {
-        colorLabel: 'success',
-        label: 'Color Label',
-        value: Date.now(),
-    },
-};
-
-export const ColorText: Story = {
-    args: {
-        colorText: '#D10DAA',
-        label: 'Color Text',
-        value: Date.now(),
-    },
-};
-
-export const Disabled: Story = {
-    args: {
-        colorText: '#D10DAA',
-        label: 'Disabled',
-        value: Date.now(),
-        disabled: true,
-    },
-};
-
-export const EndCmp_ = (args): ReactElement => (
-    <Stack spacing={3}>
-        <InputTime endCmp="Send" label="End Cmp" value={Date.now()} />
-        <InputTime endCmp={<SendIcon />} label="End Cmp" value={Date.now()} />
-    </Stack>
-);
-
-export const EndCmpExternal_ = (args): ReactElement => (
-    <Stack spacing={3}>
-        <InputTime endCmpExternal="Send" label="End Cmp External" value={Date.now()} />
-        <InputTime endCmpExternal={<SendIcon />} label="End Cmp External" value={Date.now()} />
-    </Stack>
-);
-
-export const Error: Story = {
-    args: {
-        error: true,
-        label: 'With Error',
-        value: Date.now(),
-    },
-};
-
-export const Focused: Story = {
-    args: {
-        focused: true,
-        label: 'Focused',
-        value: Date.now(),
-    },
-};
-
-export const FullWidth: Story = {
-    args: {
-        fullWidth: false,
-        label: 'Not FullWidth',
-        value: Date.now(),
+        label: 'standard variant field',
+        variant: 'standard',
     },
 };
 
 export const HelperText: Story = {
     args: {
-        helperText: 'some helperText',
-        label: 'HelperText',
-        value: Date.now(),
+        helperText: 'enter your birthdate',
     },
 };
 
-export const HideStartActionsOnEmpty_ = (args): ReactElement => (
-    <Stack spacing={3}>
-        <InputTime
-            {...args}
-            hideStartActionsOnEmpty={true}
-            startCmp="Send"
-            endCmp="Fingerprint"
-            label="Hide Start Actions OnEmpty"
-        />
-        <InputTime
-            {...args}
-            hideStartActionsOnEmpty={false}
-            startCmp="Send"
-            endCmp="Fingerprint"
-            label="Not Hide Start Actions OnEmpty"
-        />
-    </Stack>
-);
-
-export const Label: Story = {
+export const DateLimits: Story = {
     args: {
-        label: 'Some Label Input',
+        minTime: dayjs().startOf('month').toDate(),
+        maxTime: dayjs().endOf('month').toDate(),
     },
 };
 
-export const Margin_ = (args): ReactElement => (
-    <Stack>
-        <div style={{ backgroundColor: '#8d8773', textAlign: 'center' }}>some text for see the margin</div>
-        <InputTime {...args} label="None Margin" />
-        <div style={{ backgroundColor: '#8d8773', textAlign: 'center' }}>some text for see the margin</div>
-        <InputTime {...args} margin="normal" label="Normal Margin" />
-        <div style={{ backgroundColor: '#8d8773', textAlign: 'center' }}>some text for see the margin</div>
-        <InputTime {...args} margin="dense" label="Dense Margin" />
-        <div style={{ backgroundColor: '#8d8773', textAlign: 'center' }}>some text for see the margin</div>
-    </Stack>
-);
-
-export const OnChangeText: Story = {
+export const DisablePast: Story = {
     args: {
-        label: 'text field state',
-    },
-    render: (args) => {
-        // eslint-disable-next-line react-hooks/rules-of-hooks
-        const [value, setValue] = useState('');
-        return <InputTime {...args} value={value} onChange={(e) => setValue(e.target.value)} />;
+        disablePast: true,
     },
 };
 
-export const ReadOnly: Story = {
+export const DisableFuture: Story = {
     args: {
-        readOnly: true,
-        label: 'Read Only',
+        disableFuture: true,
     },
-    render: (args) => {
-        // eslint-disable-next-line react-hooks/rules-of-hooks
-        const [value, setValue] = useState('some text for show only');
-        return <InputTime {...args} value={value} onChange={(e) => setValue(e.target.value)} />;
+};
+export const DisableOpenPicker: Story = {
+    args: {
+        disableOpenPicker: true,
     },
 };
 
 export const Required: Story = {
     args: {
+        label: 'req',
         required: true,
-        label: 'Required field',
     },
 };
 
-export const StartCmp_ = (args): ReactElement => (
-    <Stack spacing={3}>
-        <InputTime {...args} startCmp="Send" label="Start Cmp" value={Date.now()} />
-        <InputTime {...args} startCmp={<SendIcon />} label="Start Cmp" value={Date.now()} />
-    </Stack>
-);
-
-export const StartCmpExternal_ = (args): ReactElement => (
-    <Stack spacing={3}>
-        <InputTime {...args} startCmpExternal="Send" label="Start Cmp External" value={Date.now()} />
-        <InputTime {...args} startCmpExternal={<SendIcon />} label="Start Cmp External" value={Date.now()} />
-    </Stack>
-);
-
-export const Variant_ = (args): ReactElement => (
-    <Stack spacing={3}>
-        <InputTime {...args} variant="filled" label="filled variant" value={Date.now()} />
-        <InputTime {...args} variant="outlined" label="outlined variant" value={Date.now()} />
-        <InputTime {...args} variant="standard" label="standard variant" value={Date.now()} />
-    </Stack>
-);
+export const PickerVariant: Story = {
+    args: {},
+    render: (args) => {
+        return (
+            <Stack spacing={2}>
+                <InputTime {...args} label="desktop picker" pickerVariant="desktop" />
+                <InputTime {...args} label="mobile picker" pickerVariant="mobile" />
+                <InputTime {...args} label="static picker" pickerVariant="static" />
+            </Stack>
+        );
+    },
+};
